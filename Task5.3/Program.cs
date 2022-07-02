4589b31e1b981d89a9deffd4e2a84d52a58ce4e3
Maksim <maksimus.tumannick@yandex.ru>
2022-07-02 23:17:33 +0300

    Task5.3 git remote add origin https://github.com/Toknnick/Task5.3.git git

---
+++ Task5.3/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Task5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isWorking = true;
+            List<int> listOfNumbers = new List<int>();
+            Console.WriteLine("sum для сложения\nexit для выхода\n");
+
+            while (isWorking)
+            {
+                string input = Console.ReadLine().ToLower().Trim();
+
+                switch (input)
+                {
+                    case "sum":
+                        int sum = 0;
+
+                        for (int i = 0; i < listOfNumbers.Count; i++)
+                            sum += listOfNumbers[i];
+
+                        Console.WriteLine($"Сумма введеных чисел: {sum}\n");
+                        break;
+                    case "exit":
+                        isWorking = false;
+                        break;
+                    default:
+
+                        if (int.TryParse(input, out int number))
+                        {
+                            listOfNumbers.Add(number);
+                            number = Convert.ToInt32(input);
+                            Console.WriteLine("Число получено.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Нужно ввести число!");
+                        }
+
+                        break;
+                }
+            }
+        }
+    }
+}
